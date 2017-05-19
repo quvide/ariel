ace514b3c5e53313aafdf5eee44aba9917d9134d
Elias Benkhodja <elias.benkhodja@gmail.com>
2017-05-19 03:03:55 +0300

    Add random quote command

--- app/main.py
+++ app/main.py
@@ -13,11 +13,20 @@ bot.add_cog(League(bot))
 from admin import Admin
 bot.add_cog(Admin(bot))
 
+import random
+
 @bot.event
 async def on_ready():
     print("Logged in")
 
-    await bot.edit_profile(username = config["username"])
-    await bot.change_presence(game = discord.Game(name = config["playing"]))
+    await bot.edit_profile(username=config["username"])
+    await bot.change_presence(game=discord.Game(name=config["playing"]))
+
+@bot.command()
+async def quote():
+    """
+    Displays a random quote
+    """
+    await bot.say(random.choice(config["quotes"]))
 
 bot.run(config["discord_token"])
